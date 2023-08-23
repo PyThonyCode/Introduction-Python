f369d4147ddb62a3b060fbe3896f8d90d701a5df
PyThonyCode <thonylp10@gmail.com>
2023-08-23 18:40:44 -0500

    actualzado

---
+++ MySessions/session4.py
@@ -0,0 +1,24 @@
+"""Nivel Básico
+1. Escribir un programa que pida al usuario una palabra y la cantidad de veces que desea que se
+muestre."""
+
+palabra = input("Ingresar palabra: ")
+repetir = int(input("Cuanto quieres repetirlo: "))
+
+for x in range(repetir):
+    print(palabra)
+
+"""
+2. Preguntar al usuario su edad, luego imprimir todos los años que ha cumplido (desde 1 hasta su
+edad).
+3. Ingresar número entero positivo, luego imprimir todos los números impares desde 1 hasta ese
+número separados por “;”.
+4. Ingresar un número entero positivo, luego imprimir la cuenta atrás desde ese número hasta
+cero separados por comas.
+5. Ingresar una cantidad a invertir, el interés anual y el número de años, luego imprima el capital
+obtenido por cada año
+6. Crear una variable que contenga un número entero y realizar un ciclo for la misma cantidad de
+ciclos
+7. Mostrar todas las palabras que introduzca el usuario hasta que el usuario escriba “salir”
+8. Crear una variable contraseña en el cual almacene lo siguiente “SENATI”, luego preguntar al
+usuario por la contraseña hasta que introduzca la contraseña correcta"""
