4145de43c5b35c1ea8cef2f21c54096ec153c9fd
PyThonyCode <thonylp10@gmail.com>
2023-08-23 16:43:49 -0500

    actualzado

--- MySessions/session3.py
+++ MySessions/session3.py
@@ -3,5 +3,13 @@ print("""
 ------ + Conjuntos -------
 ------ + Diccionarios ----""")
 
-conjunto = {1, 2, 3, "arroz"}
-print(type(conjunto))
+conjunto = {1, 2, 3, "arroz", "arroz"}
+print(len(conjunto))
+
+for x in conjunto:
+    print(x)
+
+print("arroz" in conjunto)
+conjunto.add("olla")
+
+print(conjunto)
