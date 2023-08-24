36d0758604bc75af8e67d19841db03507f6acea5
PyThonyCode <thonylp10@gmail.com>
2023-08-23 23:41:59 -0500

    actualizado

--- MySessions/session4.py
+++ MySessions/session4.py
@@ -2,23 +2,73 @@
 1. Escribir un programa que pida al usuario una palabra y la cantidad de veces que desea que se
 muestre."""
 
-palabra = input("Ingresar palabra: ")
-repetir = int(input("Cuanto quieres repetirlo: "))
-
-for x in range(repetir):
-    print(palabra)
+# palabra = input("Ingresar palabra: ")
+# repetir = int(input("Cuanto quieres repetirlo: "))
+# for x in range(repetir):
+#   print(palabra)
 
 """
 2. Preguntar al usuario su edad, luego imprimir todos los años que ha cumplido (desde 1 hasta su
-edad).
-3. Ingresar número entero positivo, luego imprimir todos los números impares desde 1 hasta ese
-número separados por “;”.
+edad)."""
+
+# usuario = int(input("Ingresar tu edad: "))
+# print("Pasastes estos años: ")
+# for x in range(1, usuario+1):
+#    print(x)
+
+""" 3. Ingresar número entero positivo, luego imprimir todos los números impares desde 1 hasta ese
+número separados por “;”."""
+
+# num = int(input("Ingresar numero: "))
+# impares = []
+# for x in range(1, num+1):
+#    if x % 2 == 1:
+#        impares.append(str(x))
+
+# print(" ; ".join(impares))
+
+""" 
 4. Ingresar un número entero positivo, luego imprimir la cuenta atrás desde ese número hasta
-cero separados por comas.
+cero separados por comas."""
+
+# num = int(input("Ingresar numero: "))
+# datos = []
+
+# while num > 0:
+
+#    datos.append(str(num))
+#    num -= 1
+
+# print(" ; ".join(datos))
+
+"""
 5. Ingresar una cantidad a invertir, el interés anual y el número de años, luego imprima el capital
-obtenido por cada año
+obtenido por cada año """
+# Pedir al usuario que ingrese la cantidad a invertir, el interés anual y el número de años
+# cantidad_invertir = float(input("Ingrese la cantidad a invertir: "))
+# interes_anual = float(input("Ingrese el interés anual (%): "))
+# num_anios = int(input("Ingrese el número de años: "))
+
+# Convertir el interés anual a decimales
+# interes_decimal = interes_anual / 100
+
+# Imprimir encabezado
+# print("Año\tCapital")
+
+# Calcular y mostrar el capital obtenido por cada año
+# for anio in range(1, num_anios + 1):
+#    capital_obtenido = cantidad_invertir * (1 + interes_decimal) ** anio
+#    print(f"{anio}\t{capital_obtenido:.2f}")
+
+"""
 6. Crear una variable que contenga un número entero y realizar un ciclo for la misma cantidad de
-ciclos
-7. Mostrar todas las palabras que introduzca el usuario hasta que el usuario escriba “salir”
+ciclos """
+
+
+"""
+7. Mostrar todas las palabras que introduzca el usuario hasta que el usuario escriba “salir” """
+
+
+"""
 8. Crear una variable contraseña en el cual almacene lo siguiente “SENATI”, luego preguntar al
 usuario por la contraseña hasta que introduzca la contraseña correcta"""
